11612d4bab92d55f232d80550ae9ca57284f0944
eriksun0310 <113155520+eriksun0310@users.noreply.github.com>
2024-04-30 17:19:04 +0800

    first commit

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,32 @@
+import { CATEGORIES } from "../data/dummy-data";
+import { FlatList } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+const CategoriesScreen = ({ navigation }) => {
+  const rendererCategoryItem = (itemData) => {
+    const pressHandler = () => {
+      navigation.navigate("MealsOverview", {
+        categoryId: itemData.item.id,
+      });
+    };
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  };
+  return (
+    <>
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item.id}
+        renderItem={rendererCategoryItem}
+        numColumns={2}
+      />
+    </>
+  );
+};
+
+export default CategoriesScreen;
---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,32 @@
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+
+const MealsOverviewScreen = ({ route }) => {
+  const id = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(id) >= 0;
+  });
+  const renderMealItem = (itemData) => {
+    return <MealItem title={itemData.item.title} />;
+  };
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
+};
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
